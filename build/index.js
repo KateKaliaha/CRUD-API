6030b793436d31ee281c196b1ff246ff08f01322
Katherina Kaliaha <mellinda077@mail.ru>
2023-01-09 12:49:03 +0300

    feat: add get request for all users

--- build/index.js
+++
@@ -1,8 +0,0 @@
-// import { a } from "./mod/a.js"
-console.log('Hi!');
-function a() {
-    console.log(process.env);
-}
-a();
-export {};
-// console.log("Bye!", a + a)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
 import * as dotenv from 'dotenv';
+
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
 dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const HOST = 'localhost';
+
+interface IUser {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
+const users: IUser[] = [];
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  if (req.url === '/api/users' && req.method === 'GET') {
+    res.statusCode = 200;
+    res.end(JSON.stringify(users));
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ message: 'User not found' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
