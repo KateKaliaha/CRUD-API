db1633108c157418df07b10c6934460b08d404f1
Katherina Kaliaha <mellinda077@mail.ru>
2023-01-13 13:29:49 +0300

    feat: add server error and gitignore file

--- src/requests/listening.ts
+++ src/requests/listening.ts
@@ -4,70 +4,75 @@ import { getAllUsers, getUserById } from './get';
 import { createUser } from './post';
 import { updateUser } from './put';
 
-export const listening = (req: IncomingMessage, res: ServerResponse) => {
-  const requestURL = req.url;
-  const requestURLSplit = (req.url as string).split('/');
+export const listening = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const requestURL = req.url;
+    const requestURLSplit = (req.url as string).split('/');
 
-  switch (req.method) {
-    case 'GET': {
-      if (requestURL === '/api/users') {
-        getAllUsers(res);
-      } else if (
-        requestURL?.startsWith('/api/users') &&
-        requestURLSplit.length === 4 &&
-        requestURLSplit[3] !== ''
-      ) {
-        const id = requestURLSplit[3];
-        getUserById(id, res);
-      } else {
+    switch (req.method) {
+      case 'GET': {
+        if (requestURL === '/api/users') {
+          getAllUsers(res);
+        } else if (
+          requestURL?.startsWith('/api/users') &&
+          requestURLSplit.length === 4 &&
+          requestURLSplit[3] !== ''
+        ) {
+          const id = requestURLSplit[3];
+          getUserById(id, res);
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Invalid request!' }));
+        }
+        break;
+      }
+
+      case 'POST': {
+        if (requestURL === '/api/users') {
+          createUser(req, res);
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Invalid request!' }));
+        }
+        break;
+      }
+
+      case 'PUT': {
+        if (
+          requestURL?.startsWith('/api/users') &&
+          requestURLSplit.length === 4 &&
+          requestURLSplit[3] !== ''
+        ) {
+          const id = requestURLSplit[3];
+          updateUser(req, res, id);
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Invalid request!' }));
+        }
+        break;
+      }
+      case 'DELETE': {
+        if (
+          requestURL?.startsWith('/api/users') &&
+          requestURLSplit.length === 4 &&
+          requestURLSplit[3] !== ''
+        ) {
+          const id = requestURLSplit[3];
+          removeUser(id, res);
+        } else {
+          res.statusCode = 404;
+          res.end(JSON.stringify({ message: 'Invalid request!' }));
+        }
+        break;
+      }
+
+      default: {
         res.statusCode = 404;
         res.end(JSON.stringify({ message: 'Invalid request!' }));
       }
-      break;
     }
-
-    case 'POST': {
-      if (requestURL === '/api/users') {
-        createUser(req, res);
-      } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'Invalid request!' }));
-      }
-      break;
-    }
-
-    case 'PUT': {
-      if (
-        requestURL?.startsWith('/api/users') &&
-        requestURLSplit.length === 4 &&
-        requestURLSplit[3] !== ''
-      ) {
-        const id = requestURLSplit[3];
-        updateUser(req, res, id);
-      } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'Invalid request!' }));
-      }
-      break;
-    }
-    case 'DELETE': {
-      if (
-        requestURL?.startsWith('/api/users') &&
-        requestURLSplit.length === 4 &&
-        requestURLSplit[3] !== ''
-      ) {
-        const id = requestURLSplit[3];
-        removeUser(id, res);
-      } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ message: 'Invalid request!' }));
-      }
-      break;
-    }
-
-    default: {
-      res.statusCode = 404;
-      res.end(JSON.stringify({ message: 'Invalid request!' }));
-    }
+  } catch {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ message: 'Internal Server Error. Try later!' }));
   }
 };
